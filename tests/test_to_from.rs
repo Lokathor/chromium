ed873f4c840da9f58f487476087f519785440793
Lokathor <zefria@gmail.com>
2020-03-06 16:00:44 -0700

    More testing

--- tests/test_to_from.rs
+++ tests/test_to_from.rs
@@ -21,3 +21,22 @@ fn test_UniqueSlice_to_from() {
   let back_as_a_slice: &mut [i32] = c.into();
   assert_eq!(back_as_a_slice, &mut [1,2,3,4]);
 }
+
+#[test]
+fn test_SharedStr_to_from() {
+  let my_str: &str = "hello";
+  let s = SharedStr::from(my_str);
+  assert_eq!(s.deref(), "hello");
+  let back_as_a_str: &str = s.into();
+  assert_eq!(back_as_a_str, "hello");
+}
+
+#[test]
+fn test_UniqueStr_to_from() {
+  let mut hello = String::from("hello");
+  let my_str: &mut str = hello.deref_mut();
+  let mut s = UniqueStr::from(my_str);
+  assert_eq!(s.deref_mut(), "hello");
+  let back_as_a_str: &mut str = s.into();
+  assert_eq!(back_as_a_str, "hello");
+}
